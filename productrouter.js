f647ee456d613be111ed5cf1dc288caf1b4f03c8
Chitsanupong Voravijitchaikul <chit.1@hotmail.com>
2019-11-01 21:08:50 +0700

    add get and post method

--- productrouter.js
+++ productrouter.js
@@ -1,4 +1,3 @@
-
 var express = require("express");
 var router = express.Router();
 var Product = require("./models/product");
@@ -19,12 +18,56 @@ router.get("/:_id", (req, res) => {
   });
 });
 
+// GET by price gte
+router.get("/price/gte/:num", (req, res) => {
+  const query = { "price": { "$gte": req.params.num } };
+  Product.find(query).exec((err, data) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).send(data);
+  });
+});
+
+// GET by price gte
+router.get("/price/lte/:num", (req, res) => {
+  const query = { "price": { "$lte": req.params.num } };
+  Product.find(query).exec((err, data) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).send(data);
+  });
+});
+
+
+// GET by name
+router.get("/name/:name", (req, res) => {
+  const query = {'name': {'$regex': req.params.name }};
+  Product.find(query).exec((err, data) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).send(data);
+  });
+});
+
 // POST (create new data)
 router.post("/", (req, res) => {
   var obj = new Product(req.body);
   obj.save((err, data) => {
     if (err) return res.status(400).send(err);
     res.status(200).send("Add success");
+  });
+});
+
+// PUT (update current data)
+router.post("/put/:_id", (req, res) => {
+  Product.findByIdAndUpdate(req.params._id, { $set: req.body }, (err, data) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).send("Update success");
+  });
+});
+
+// DELETE (delete 1 data)
+router.post("/delete/:_id", (req, res) => {
+  Product.findByIdAndDelete(req.params._id, (err, data) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).send("Delete success");
   });
 });
 
