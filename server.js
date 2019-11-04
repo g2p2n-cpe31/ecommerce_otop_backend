403a4104ada8303be531b4cf4659143817cd53f6
Chitsanupong Voravijitchaikul <chit.1@hotmail.com>
2019-11-04 21:05:42 +0700

    fix server.js

--- server.js
+++ server.js
@@ -29,6 +29,12 @@ app.listen(port, () => {
   console.log('[success] task 1 : listening on port ', port);
 });
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.status(200).send("first page of api express");
 });
