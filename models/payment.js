51ae23218aab86382fbfaac0759402a82baa6ff7
Chitsanupong Voravijitchaikul <chit.1@hotmail.com>
2019-11-05 18:36:06 +0700

    update schema

--- models/payment.js
+++ models/payment.js
@@ -5,11 +5,11 @@ const paymentSchema = new Schema({
   orderId: String,
   payBy: String,
   shipOption: String,
-  shippingProvince: [String],
-  shippingCountry: [String],
-  shippingDistrict: [String],
-  shippingPostalNumber: [String],
-  shippingAddress: [String]
+  shippingProvince: String,
+  shippingCountry: String,
+  shippingDistrict: String,
+  shippingPostalNumber: String,
+  shippingAddress: String
 }, { timestamps: true, versionKey: false })
 
 const PaymentModel = mongoose.model('Payment', paymentSchema)
--- models/product.js
+++ models/product.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const productSchema = new Schema({
   name: String,
-  productDetail: String,
+  detail: String,
   storeId: String,
   owner: String,
   price: Number,
